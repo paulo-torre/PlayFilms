a7893d14886c6c8d04bcb305747599f4ebe4dc0b
Marcelo Torre <pauloricardo.torre@gmail.com>
2020-08-03 18:59:15 -0300

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a>
+      </a>
+      <p>
+        ©PlayFilm Corporation
+        {' '}
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
